a2997db78652fcf1cd16f578870bef051f7304db
강한길 <59948163+gksrlf@users.noreply.github.com>
2022-06-22 13:17:43 +0900

    싱글톤패턴

---
+++ src/ChocolateBoiler/ChocolateBoiler.java
@@ -0,0 +1,50 @@
+package ChocolateBoiler;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    private static ChocolateBoiler uniqueInstance;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if(uniqueInstance == null) {
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
+    }
+
+    public void fill() {
+        if(isEmpty()) {
+            empty = false;
+            boiled = false;
+            // 보일러에 우유와 초콜릿을 혼합한 재료를 넣음
+        }
+    }
+
+    public void drain() {
+        if(!isEmpty() && !isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if(!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+
+}
+
+// 회색은 빈칸의 코드
